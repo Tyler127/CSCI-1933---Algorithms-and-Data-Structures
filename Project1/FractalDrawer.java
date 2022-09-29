8cfd19db0e45ebb5ca51d226c0db4703ea034feb
Connor <cpatrickkanten@gmail.com>
2022-09-29 17:02:54 -0500

    Rectangle and Circle Fractals

--- Project1/FractalDrawer.java
+++ Project1/FractalDrawer.java
@@ -13,7 +13,7 @@ public class FractalDrawer {
     // and determines which shapes to draw a fractal by calling appropriate helper function
     // drawFractal returns the area of the fractal
     public double drawFractal(String type) {
-        Canvas theCanvas = new Canvas(800, 800);
+        Canvas theCanvas = new Canvas(1600, 1600);
 
         if (type.equals("triangle")) {
             System.out.println("making triangle");
@@ -22,10 +22,10 @@ public class FractalDrawer {
             return totalArea;
         }
         else if (type.equals("circle")) {
-            drawCircleFractal(200, 400, 400, theCanvas, 7); 
+            drawCircleFractal(100, 400, 400, theCanvas, 7); 
         }
         else if (type.equals("rectangle")) {
-            drawRectangleFractal(165, 300, 400, 400, theCanvas, 7);
+            drawRectangleFractal(75, 150, 400, 400, theCanvas, 7);
         }
         return 0.0;
     }
@@ -62,18 +62,55 @@ public class FractalDrawer {
     // TODO:
     // drawCircleFractal draws a circle fractal using recursive techniques
     public void drawCircleFractal(double radius, double x, double y, Canvas can, int level) {
+        // RGB Values
+        // Idea 3 used here
+        int red = 40 + (int)(Math.random() * 120);
+        int blue = 30 + (int)(Math.random() * 100);
+        int green = 30 + (int)(Math.random() * 100);
+
         Circle theCircle = new Circle(x, y, radius);
-        theCircle.setColor(255, 0, 0);
+        theCircle.setColor(red, green, blue);
         can.drawShape(theCircle);
+
+         // Recursion to make 4 more circles from parent until level = 0
+         if (level == 0) {
+            return;
+        }
+        else {
+            // Idea 2 used here
+            drawCircleFractal(radius/2, x, y - (radius + (radius/2)),can,level - 1);//top
+            drawCircleFractal(radius/2, x, y + (radius + (radius/2)),can,level - 1);//bottom
+            drawCircleFractal(radius/2, x - (radius + (radius/2)), y,can,level - 1);//left
+            drawCircleFractal(radius/2, x + (radius + (radius/2)), y,can,level - 1);//right
+        }
     }
 
 
     //TODO:
     // drawRectangleFractal draws a rectangle fractal using recursive techniques
     public void drawRectangleFractal(double width, double height, double x, double y, Canvas can, int level) {
+        // RGB Values
+        // Idea 3 used here
+        int red = 40 + (int)(Math.random() * 120);
+        int blue = 30 + (int)(Math.random() * 100);
+        int green = 30 + (int)(Math.random() * 100);
+
+
         Rectangle theRectangle = new Rectangle(x, y, width, height);
-        theRectangle.setColor(255,0,0);
+        theRectangle.setColor(red,green,blue);
         can.drawShape(theRectangle);
+
+        // Recursion to make 4 more rectangles from parent until level = 0
+        if (level == 0) {
+            return;
+        }
+        else {
+            // Idea 2 used here
+            drawRectangleFractal(width/2, height/2, x - (width/2),  y - (height/2), can, level-1);//upper left
+            drawRectangleFractal(width/2, height/2, x - (width/2),  y + (height), can, level-1);//lower left
+            drawRectangleFractal(width/2, height/2, x + width,  y - height/2, can, level-1);//upper right
+            drawRectangleFractal(width/2, height/2, x + width,  y + height, can, level-1);//lower right
+        }
     }
 
     //TODO:
