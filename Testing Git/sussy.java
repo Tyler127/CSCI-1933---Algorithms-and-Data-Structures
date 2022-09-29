1273fe7b3f6c23b8d06c55b1351838f0a5de43e9
Tyler <2002tjlarson2@gmail.com>
2022-09-29 04:00:17 -0500

    Removed Testing Git files

--- Testing Git/sussy.java
+++
@@ -1,9 +0,0 @@
-public class sussy {
-    int x = 0;
-
-
-    int y = 3;
-    public void susclass () {
-
-    }
-}
--- Testing Git/testnumber2.java
+++
@@ -1,5 +0,0 @@
-public class testnumber2 {
-    int i = 0;
-    String test = "conna sussy moment confirmed";
-    String test2 ="test commit numba 2";
-}
