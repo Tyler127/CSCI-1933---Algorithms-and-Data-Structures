8f415eb138424784a01cf124e232e760250e67a1
Connor <cpatrickkanten@gmail.com>
2022-10-13 16:50:48 -0500

    milestone3 finished

--- Labs/lab5/BookShelfReader.java
+++ Labs/lab5/BookShelfReader.java
@@ -13,28 +13,38 @@ public class BookShelfReader {
             s = new Scanner(new File(fileName));
             while (s.hasNextLine()){
                 String line = s.nextLine();//doesn't need to split on \n, already only reads 1 line
-                System.out.println(line);
+                //System.out.println(line);
                 String[] splitted = line.split(", ");
                 String title = splitted[0];
                 String author = splitted[1];
-                System.out.println(splitted[2]);
-                int rating = 0;//Integer.parseInt(splitted[2]);
+                Double rating = Double.parseDouble(splitted[2]);
                 Book currBook = new Book(title, author, rating);
-                System.out.println(currBook.toString());
                 bookShelfEins.add(currBook);
             }
             return bookShelfEins;
             
         } catch (Exception e){
-            System.out.println("Exception Found");
+            System.out.println("Exception Found,  this is the file exception");
             return null;
         }
 
+    }
+    public static void writeShelfToFile(Bookshelf b, String fileName){
+        PrintWriter p = null; // declare p outside try-catch block
+        try {
+            p = new PrintWriter(new File(fileName));
+            p.print(b.toString());
+            p.print("\n");
+            p.close();//if you do not close the file, the output file will remain blank
+        } catch (Exception e) {
+            System.out.println("Exception found");
+        }
     }
 
     public static void main(String[] args){
         Bookshelf x = readBooksFromFile("bookinput.txt");
         //System.out.println(x.toString());
+        writeShelfToFile(x, "output.txt");
     }
 
 }
