574ec2a11ed56484742949ec624376a86bde83d8
Tyler <2002tjlarson2@gmail.com>
2022-12-06 01:57:19 -0600

    hamshbrown

--- src/HashTable.java
+++ src/HashTable.java
@@ -4,25 +4,117 @@ import java.util.Scanner;
 
 public class HashTable<T>{
     NGen<T>[] hashTable;
+    public String type = "GENERAL";
 
-    //TODO: Create a constructor that takes in a length and initializes the hash table array
+    public HashTable(int length) {
+        this.hashTable = new NGen[length];
+        for (int i = 0; i < length; i++) hashTable[i] = new NGen<T>();
+    }
 
-    //TODO: Implement a simple hash function
     public int hash1(T item) {
-        return -1;
+        /*
+        Multiplicative hashing: (usually on numbers but used the concept of coprime numbers here)
+        Standard multiplicative hashing uses the formula:
+        hash(K) = (aK mod W) / (W/table length)
+        which produces a hash value in {0,...,table length - 1}.
+        The value 'a' is an appropriately chosen value that should be relatively prime to 'W';
+        it should be large and its binary representation a random mix of 1's and 0's.
+         */
+
+        int hashReturn = 13;
+        if (item instanceof String) {
+            String[] chars = ((String) item).split("");
+            for (String string : chars) {
+                hashReturn = (hashReturn * string.charAt(0)) / 23;
+
+                hashReturn = hashReturn * 12;
+                hashReturn = hashReturn / 13;
+
+                hashReturn = hashReturn * 23;
+                hashReturn = hashReturn / 5;
+            }
+
+            // Mod hash return, so it is always within the index range of the hashTable
+            hashReturn = hashReturn % hashTable.length;
+
+            // If the hash function went over the int max value, take absolute value of the mod
+            if (hashReturn < 0) hashReturn = Math.abs(hashReturn);
+
+            //System.out.println("Hash Return: " + hashReturn + "\n");
+        }
+        return hashReturn;
     }
 
     //TODO: Implement a second (different and improved) hash function
     public int hash2(T item) {
-        return -1;
+        int hashReturn = 0;
+        if (item instanceof String) {
+            String[] chars = ((String) item).split("");
+            for (String string : chars) {
+                hashReturn += string.charAt(0);
+                hashReturn = hashReturn * 97879;
+                hashReturn = hashReturn * 999451;
+                //hashReturn = hashReturn ^ string.charAt(0) >>> 3;
+            }
+
+            // Mod hash return, so it is always within the index range of the hashTable
+            hashReturn = hashReturn % hashTable.length;
+
+            // If the hash function went over the int max value, take absolute value of the mod
+            if (hashReturn < 0) hashReturn = Math.abs(hashReturn);
+        }
+        return hashReturn;
     }
 
     //TODO: Implement the add method which adds an item to the hash table using your best performing hash function
     // Does NOT add duplicate items
     public void add(T item) {
+        // Case 1: Item is null
+        if (item == null) return;
+
+        // Decide which hash function to use based on type
+        NGen<T> head = null;
+        int hashValue = 0;
+        if (this.type.equals("GENERAL")) {
+            hashValue = this.hash1(item);
+            head = this.hashTable[hashValue];
+        }
+        else if (this.type.equals("SPECIFIC")) {
+            hashValue = this.hash2(item);
+            head = this.hashTable[hashValue];
+        }
+
+        // Case 2: head points to nothing
+        if (head.getNext() == null) head.setNext(new NGen<>(item, null));
+
+        // Case 3: Loop until current node next is null and add new node
+        else {
+            NGen<T> pointer = head.getNext();
+
+            while (pointer.getNext() != null) {
+                //System.out.println("    in while loop:");
+                //System.out.println("        current node data: " + pointer.getData());
+                //System.out.println("        item data: " + item);
+
+                // Return if the pointer's data is equal to item
+                if (pointer.getData().equals(item)) return;
+
+                pointer = pointer.getNext();
+            }
+
+            // Check again for equivalence when pointer.getNext() == null,
+            // as this case isn't included in the while loop
+            if (pointer.getData().equals(item)) {
+                //System.out.println("    after while - current node == item: " + pointer.getData().equals(item));
+                //System.out.println("\n");
+                return;
+            }
+            //System.out.println("            Case 3 - new node added with data: " + item);
+            pointer.setNext(new NGen<>(item, null));
+
+        }
     }
 
-    // ** Already implemented -- no need to change **
     // Adds all words from a given file to the hash table using the add(T item) method above
     @SuppressWarnings("unchecked")
     public void addWordsFromFile(String fileName) {
@@ -50,10 +142,46 @@ public class HashTable<T>{
     // - average collision length
     // - length of longest chain
     public void display() {
+        int numUniqueWords = 0;
+        int longestChain = 0;
+        int numEmptyIndices = 0;
+        int numNonEmptyIndices;
+
+        for (NGen<T> head : this.hashTable) {
+            // Case 1: Head next is null.
+            if (head.getNext() == null) numEmptyIndices++;
+
+            // Case 2: Count list length and check item uniqueness
+            else {
+                NGen<T> pointer = head.getNext();
+                int counter = 1;
+                numUniqueWords++;
+
+                while (pointer.getNext() != null) {
+                    counter++;
+                    numUniqueWords++;
+                    pointer = pointer.getNext();
+                }
+
+                // Reassign longestChain if this list had more items
+                if (counter > longestChain) longestChain = counter;
+            }
+        }
+        System.out.println("Number of Unique Words: " + numUniqueWords);
+        System.out.println("Number of Empty Indices: " + numEmptyIndices);
+        numNonEmptyIndices = this.hashTable.length - numEmptyIndices;
+        System.out.println("Number of Non-Empty Indices: " + numNonEmptyIndices);
+        System.out.println("Average Collision Length: " + (float) numUniqueWords/numNonEmptyIndices);
+        System.out.println("Length of Longest Chain: " + longestChain);
     }
+
+    public NGen<T>[] getHashTable() {return this.hashTable;}
+
 
     // TODO: Create a hash table, store all words from "canterbury.txt", and display the table
     //  Create another hash table, store all words from "keywords.txt", and display the table
     public static void main(String args[]) {
+
+
     }
 }
